7b6003ff75201ebb7077d3a133b0d01140e54039
patofox13 <121024435+patofox13@users.noreply.github.com>
2022-12-21 18:58:12 +0100

    Update docker.sh

--- terraform/projects/docker/scripts/docker.sh
+++ terraform/projects/docker/scripts/docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cat << EOF >> /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1 
+EOF
+
+sysctl -p
+
+apt -y update 
+
+apt -y upgrade
+
 apt -y remove docker docker-engine docker.io containerd runc
 
 apt-get install \
